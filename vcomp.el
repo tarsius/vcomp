fdd010e9081d62aa6aaa1b25a2df925efd662d0c
Jonas Bernoulli <jonas@bernoul.li>
2023-04-07 16:26:29 +0200

    List FSF as a copyright holder
    This is partially based on an early implementation in "package.el".

--- vcomp.el
+++ vcomp.el
@@ -1,6 +1,7 @@
 ;;; vcomp.el --- Compare version strings  -*- lexical-binding:t -*-
 
 ;; Copyright (C) 2008-2023 Jonas Bernoulli
+;; Copyright (C) 2007-2008 Free Software Foundation, Inc.
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/vcomp
